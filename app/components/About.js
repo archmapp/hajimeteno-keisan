f656852cecbf65679e36674325f67bb2c136b8b9
ty7575au <ty7575au@yahoo.co.jp>
2021-10-29 22:21:50 +0900

    li text-align

--- app/components/About.js
+++ app/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const About = (props) => {
 	return (
-		<article className="container box py-2">
+		<article className="articleAbout container box py-2">
 			<div className="columns is-centered">
 				<div className="column is-12 content">
 					<article className="message is-info">
@@ -35,21 +35,25 @@ const About = (props) => {
 										<li>
 											<a
 												href="https://www.youtube.com/watch?v=EVze4Cq-dZ8"
-												className=""
+												className="has-text-left"
 											>
-												<span className="is-size-6 has-text-success-dark ">
+												<span className="is-size-6 has-text-success-dark">
 													JavaScriptゲーム：CSSトランジションとアニメーション
-													<br />
 													を使用したインタラクティブな質問 へ 移動
 												</span>
 											</a>
 										</li>
 										<li>
-											<a
-												href="https://www.youtube.com/watch?v=eddzBlXBl3Y"
-												className=""
-											>
-												<span className="is-size-6 has-text-success-dark ">
+											<a href="https://www.youtube.com/watch?v=70fadMRqnBo">
+												<span className="is-size-6 has-text-success-dark">
+													Learn React.js in 70 Minutes: JSX &amp; useState
+													Tutorial へ 移動
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="https://www.youtube.com/watch?v=eddzBlXBl3Y">
+												<span className="is-size-6 has-text-success-dark">
 													React (Babel) and Sass webpack Tutorial へ 移動
 												</span>
 											</a>
@@ -73,11 +77,8 @@ const About = (props) => {
 								<div className="mt-3 ml-3">
 									<ul>
 										<li>
-											<a
-												href="https://www.youtube.com/watch?v=w7ejDZ8SWv8"
-												className=""
-											>
-												<span className="is-size-6 has-text-success-dark ">
+											<a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8">
+												<span className="is-size-6 has-text-success-dark">
 													React JS Crash Course 2021 へ 移動
 												</span>
 											</a>
